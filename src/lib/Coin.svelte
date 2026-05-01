05004a9a613d6e909d301a9b4fce2d17f585f02f
teterw <teteemail27194@gmail.com>
2026-05-01 14:45:40 +0700

    feat:localstorage

--- src/lib/Coin.svelte
+++ src/lib/Coin.svelte
@@ -1,6 +1,13 @@
 <script>
     import yen from "$lib/assets/yen.png";
-    let coin = $state(0)
+    check = false
+    if (check == false){
+        localStorage.setItem("newvalue",0)
+        localStorage.setItem("currentvalue",0)
+        check = true
+    }
+    let coin = $state(localStorage.getItem("currentvalue"))
+
 </script>
 
 <div class="border fixed bottom-0 w-1/2">
@@ -14,7 +21,7 @@
             class="bg-black border w-70 h-20 rounded-full m-4 flex justify-end drop-shadow-xl/50"
         >
             <div class="text-[#FFB957] items-center flex mb-2">
-                <h1 class="font-bold text-4xl text-shadow-2xs text-shadow-[#FFB957]">0</h1>
+                <h1 class="font-bold text-4xl text-shadow-2xs text-shadow-[#FFB957]">{coin}</h1>
             </div>
             <div class="justify-end flex items-center">
                 <img src={yen} alt="" class="w-20 h-20 m-2 mb-4.5" />
