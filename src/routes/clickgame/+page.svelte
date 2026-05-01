184484a7041dbeff05f4ff8a6f40c055b794ed73
Ji-69 <tecno26601@gmail.com>
2026-05-01 14:12:02 +0700

    complete

--- src/routes/clickgame/+page.svelte
+++ src/routes/clickgame/+page.svelte
@@ -1,31 +1,69 @@
 <script>
-    let counter =$state(0)
-    let index = $state(0)
+   import chichipop from '$lib/assets/chichi-pop.png'
+   import catup from '$lib/assets/catup.png'
+   import catdown from '$lib/assets/catdown.png'
+    let counter = $state(0)
+    let coins = $state(0)
 
-    const increment =()=>{
-        counter +=1
-        if (index === 0) {
+    
+    let images = [
+      catup,
+      catdown
+    ];
+    let currentIndex = $state(0);
+    
+    function toggleImage() {
+      currentIndex = (currentIndex + 1) % images.length;
 
-        }
+      
+      if (counter %100 === 0 ) {
+       coins += 10;
+      }
     }
-
- let images = [
-    'https://img.freepik.com/free-vector/english-alphabets_1308-2901.jpg?semt=ais_hybrid&w=740&q=80',
-    'https://m.media-amazon.com/images/I/71VdS8tkVNL.jpg'
-  ];
-  let currentIndex = 0;
-
-  function toggleImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-  }
+    const increment =()=>{ 
+        counter +=1
+        toggleImage() 
+    }
 
 </script>
 
 
-<p>{counter}</p>
+
+<div class="justify-center flex items-center  bg-cover h-screen bg-center  " style="
+background-image: url({chichipop}); ">
 
 
-<button onclick={increment}> Tab </button>
+<div class="absolute bottom-0 ">
+  
+  <div class="box">
+  <p>score = {counter}</p>
+  
+</div>
 
-<a href="#"  >
-    <button> Go back to lobby</button></a>
+
+
+<style>
+.box  {
+  position:clearInterval;
+  background-color:white;
+  padding:10px;
+  border-radius:50px;
+  font-size:50px;
+  
+
+}
+</style>
+
+
+
+  
+  <div  class="cursor-pointer">
+    <img class="h-[400px] w-[400px]" alt=""  src ='{images[currentIndex]}'  onclick={increment}/>
+  </div>
+  <!-- <a href="/"  >
+      <button>กลับ</button></a> -->
+</div>
+
+
+
+</div>
